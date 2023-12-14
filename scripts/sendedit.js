9e7f23f42b6cb537cb074a8efd2d76f48acf1ffd
Dario Ferreira <fcdario@gmail.com>
2023-12-14 15:15:52 -0300

    Formulario 'editar.html' funcional

--- scripts/sendedit.js
+++ scripts/sendedit.js
@@ -1,35 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
+//document.addEventListener('DOMContentLoaded', function () {
+  let url = "https://darferreira.pythonanywhere.com"
     // lee los argumentos pasados a este formulario
     //separa el string por los “&” creando una lista [“id=3” , “nombre=’tv50’” , ”precio=1200”,”stock=20”]
     var args = location.search.substr(1).split('&'); 
-    console.log(args)
+    //console.log(args)
     var parts = []
     for (let i = 0; i < args.length; ++i) {
       parts[i] = args[i].split('=');
     }
-    //decodeUriComponent elimina los caracteres especiales que recibe en la URL 
-    document.getElementById("nombre").value = decodeURIComponent(parts[1][1])
-    document.getElementById("precio").value = decodeURIComponent(parts[2][1])
-    document.getElementById("stock").value =decodeURIComponent( parts[3][1])
-    document.getElementById("descripcion").value =decodeURIComponent( parts[4][1])
-    document.getElementById("archivo").value =decodeURIComponent( parts[5][1])
-
+    //console.log(parts[0])
+    var id = decodeURIComponent(parts[0][1]);
     var nombreInput = document.getElementById('nombre');
     var descripcionInput = document.getElementById('descripcion');
     var precioInput = document.getElementById('precio');
     var stockInput = document.getElementById('stock');
     var archivoInput = document.getElementById('archivo');
     var cargarProductoBtn = document.getElementById('cargarProducto');
+    var cargarImagePreview = document.getElementById("preview-image")
+
+    // Agregar eventos de entrada para cada campo
+    nombreInput.addEventListener('input', checkFormCompletion);
+    descripcionInput.addEventListener('input', checkFormCompletion);
+    precioInput.addEventListener('input', checkFormCompletion);
+    stockInput.addEventListener('input', checkFormCompletion);
+    //archivoInput.addEventListener('input', checkFormCompletion);
+
+    //decodeUriComponent elimina los caracteres especiales que recibe en la URL 
+    nombreInput.value = decodeURIComponent(parts[1][1])
+    precioInput.value = decodeURIComponent(parts[2][1])
+    stockInput.value =decodeURIComponent( parts[3][1])
+    descripcionInput.value =decodeURIComponent( parts[4][1])
+    cargarImagePreview.setAttribute('src',`${url}/${decodeURIComponent( parts[5][1])}`)
+    // =decodeURIComponent( parts[5][1])
 
     function checkFormCompletion() {
       var nombre = nombreInput.value;
       var descripcion = descripcionInput.value;
       var precio = precioInput.value;
       var stock = stockInput.value;
-      var archivo = archivoInput.value;
+      //var archivo = archivoInput.value;
 
       // Verificar si todos los campos están completos
-      if (nombre && descripcion && precio && stock && archivo) {
+      if (nombre && descripcion && precio && stock) {
         // Habilitar el botón si todos los campos están completos
         cargarProductoBtn.disabled = false;
       } else {
@@ -38,24 +50,47 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
 
-    // Agregar eventos de entrada para cada campo
-    nombreInput.addEventListener('input', checkFormCompletion);
-    descripcionInput.addEventListener('input', checkFormCompletion);
-    precioInput.addEventListener('input', checkFormCompletion);
-    stockInput.addEventListener('input', checkFormCompletion);
-    archivoInput.addEventListener('input', checkFormCompletion);
-});
+
+
+//});
+
+function previewImage() {
+  var input = document.getElementById('archivo');
+  var preview = document.getElementById('preview-image');
+
+  var file = input.files[0];
+
+  if (file) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          preview.src = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+  } else {
+      preview.src = '';
+  }
+}  
 
 function enviarDatos() {
-  let url = "https://darferreira.pythonanywhere.com"
+  //let url = "https://darferreira.pythonanywhere.com"
     document.getElementById('notificaciones').innerHTML ="";
-    var id = document.getElementById("id");
+    //var id = document.getElementById("id");
     var nombre = document.getElementById('nombre').value;
     var descripcion = document.getElementById('descripcion').value;
     var precio = document.getElementById('precio').value;
     var stock = document.getElementById('stock').value;
     var input = document.getElementById('archivo');
     var archivo = input.files[0];
+    // Si el input=file estaba vacío creo un nuevo File con el atributo filename=''
+    if (input.files.length == 0){
+      archivo = new File([""],'');
+      console.log("input esta vacío!!!")
+    }
+
+    console.log(archivo);
+    
 
     var formData = new FormData();
     formData.append('imagen', archivo);
@@ -66,7 +101,7 @@ function enviarDatos() {
 
 // console.log(formData);
     // Realizar la solicitud POST a la API REST
-    fetch(`${url}/api/productos`+id, {
+    fetch(`${url}/api/productos/`+id, {
         method: 'PUT',
         body: formData
     })
